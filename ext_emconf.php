4d304213286747481095119f561987e282ee1910
Jaume Presas <jpresas@w-4.com>
2023-09-26 11:27:20 +0200

    [FIX] Fixed checkboxes positioning bug.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'W4 Cookiebar',
     'description' => 'W4 extension to provide a GDPR (DE: "DSGVO") cookie banner.',
-    'version' => '1.0.6',
+    'version' => '1.0.7',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
